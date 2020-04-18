48c1408314eb7d08b14621d436c1eac7fd1146d8
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-18 16:09:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,4 +1,8 @@
-# prereqs: iterators, hashes, conditional logic
+name_hash = {
+  :ace => 27, 
+  :beta => 2, 
+  :charlie => 33
+}# prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)

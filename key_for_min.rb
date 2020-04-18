f6d7deca6537f8e668fd6e55f77e63a4c7bfa022
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-18 16:19:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 name_hash = {
-  :ace => 27,
-  :beta => 2,
+  :alpha => 27,
+  :bravo => 2,
   :charlie => 33
 }# prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
   name_hash.collect do |word, num|
     if num == 0
       return num
-    else num.first
+    elsif num <= 10
       num
     end
   end
